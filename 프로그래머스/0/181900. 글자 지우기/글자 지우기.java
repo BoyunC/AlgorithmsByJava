2094e066d51040c83b51ea61f046a228c43e5395
BoYun <cbyn0819@gmail.com>
2024-06-17 16:28:37 +0900

    [level 0] Title: 글자 지우기, Time: 15.93 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        int idx = 0;
+        
+        Arrays.sort(indices);
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            if(idx < indices.length && i == indices[idx]) {
+                idx++;
+                continue;
+            } 
+            
+            answer += my_string.charAt(i);
+        }
+        
+        return answer;
+    }
+}
