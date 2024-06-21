52b3851aed3b54355b1c37fd03215077e049e5fd
BoYun <cbyn0819@gmail.com>
2024-06-21 17:26:13 +0900

    [level 0] Title: l로 만들기, Time: 14.93 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,15 +1,6 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
         
-        for (char c : myString.toCharArray()) {
-            if ( c < 'l')
-                answer += 'l';
-            else 
-                answer += c;
-        }
-        
-        
-        return answer;
+        return myString.replaceAll("[a-l]", "l");
     }
 }
