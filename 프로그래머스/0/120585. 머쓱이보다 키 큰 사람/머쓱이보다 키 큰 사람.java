baacf53d8ed4f5806381b1b5dfd2f02d209e5e74
BoYun <cbyn0819@gmail.com>
2024-06-25 12:59:06 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.42 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.Collections;
 
 class Solution {
     public int solution(int[] array, int height) {
